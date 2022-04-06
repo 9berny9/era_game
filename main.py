8d12383a45df0b7303dba9fc57b9f498380294d1
9berny9 <bernatik.michal@gmail.com>
2022-04-06 16:22:49 +0200

    edit formating variables

--- main.py
+++ main.py
@@ -195,22 +195,19 @@ class Game:
             self.checkEndCondition()
 
         else:
-            print(
-                "Game ended, {p1name} has {p1wins} wins, and {p2name} has {p2wins} wins".format(p1name=self.firstPlayer.name,
-                                                                                      p1wins=self.firstPlayer.winsNumber,
-                                                                                      p2name=self.secondPlayer.name,
-                                                                                      p2wins=self.secondPlayer.winsNumber))
+            print(f"""Game ended, {self.firstPlayer.name} has {self.firstPlayer.winsNumber} wins, and {self.secondPlayer.name} has {self.secondPlayer.winsNumber} wins""")
             self.determineWinner()
             self.endGame = True
 
     def determineWinner(self):
-        resultString = "It's a Draw"
+        result_string = "It's a Draw"
         if self.firstPlayer.winsNumber > self.secondPlayer.winsNumber:
-            resultString = "Winner is {name}".format(name=self.firstPlayer.name)
+            result_string = "Winner is {name}".format(name=self.firstPlayer.name)
         elif self.firstPlayer.winsNumber < self.secondPlayer.winsNumber:
-            resultString = "Winner is {name}".format(name=self.secondPlayer.name)
+            result_string = "Winner is {name}".format(name=self.secondPlayer.name)
+        print(result_string)
 
-        print(resultString)
+
 
 
 game = Game()
@@ -218,35 +215,7 @@ game.start()
 
 
 
-def welcome_and_rules():
-    '''
-    Function print welcome and rules for two players.
-    '''
-    welcome = f"""
-WELCOME TO ERA GAME!
-{separator(30)}
-GAME DESCRIPTION:
-ERA Game is turn-based strategy game between two players. 
-There are 16 unique stones with 4 characteristics, which are placed into 4x4 board. 
-All stones are visible to both players. 
-Player, who completes four stones with at least one same characteristic in row, column or diagonal, is winner.
-{separator(30)}
-RULES:
-Game begins by Player 1 choosing any stone from all 16 free stones. 
-This chosen stone Player 1 gives to Player 2. Player 2 places the stone somewhere on the board.
-Then Player 2 chooses any stone from 15 free stones and gives it to Player 1. Player 1 places stone somewhere on the board.
-Winning row/column/diagonal is row of four stones with at least one same characteristic.
-Both players take turns until all stones are placed or one of them wins.
-{separator(30)}
-GAME FIELD:
-{game_board()}
 
-
-{separator(30)}
-Let's start the game!
-{separator(30)}"""
-
-    return print(welcome)
 
 
 def separator(delka=45):

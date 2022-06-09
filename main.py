2871dbbff8530d1d1ab12885e93c9da806285e6c
Michal B <96014046+9berny9@users.noreply.github.com>
2022-06-09 14:41:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -358,9 +358,9 @@ class Game:
         """
         welcome = f"""
 {self.separator(20)}
-WELCOME TO ERA GAME!
+WELCOME TO QUARTO GAME!
 {self.separator(20)}
-ERA Game is turn-based strategy game between two players. 
+QUARTO Game is turn-based strategy game between two players. 
 There are 16 unique stones with 4 characteristics, which are placed into 4x4 board. 
 All stones are visible to both players. 
 Player, who completes four stones with at least one same characteristic in row, column or diagonal, is winner.
